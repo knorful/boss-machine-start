f71c1b951df1c9ac037b8d78ef806ebc868fb6f0
Kris Norful <krisnorful@gmail.com>
2021-05-05 20:01:37 -0500

    add route to get all minions from database

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const app = express();
 
 module.exports = app;
@@ -11,8 +9,12 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
+const cors = require("cors");
+app.use(cors());
 
 // Add middware for parsing request bodies here:
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require("./server/api");
@@ -22,4 +24,7 @@ app.use("/api", apiRouter);
 // This conditional is here for testing purposes:
 if (!module.parent) {
   // Add your code to start the server listening at PORT below:
+  app.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`);
+  });
 }
--- server/api.js
+++ server/api.js
@@ -1,4 +1,14 @@
 const express = require("express");
+const db = require("./db");
 const apiRouter = express.Router();
+
+apiRouter.get("/minions", (req, res, next) => {
+  const allMinions = db.getAllFromDatabase("minions");
+  if (allMinions) {
+    res.status(200).send(allMinions);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 module.exports = apiRouter;
